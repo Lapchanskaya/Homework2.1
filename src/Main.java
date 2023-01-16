17c2d94cfb3ce632de65054abc0960bebfbf3ccc
Svetlana <isaeva322@mail.ru>
2023-01-17 00:42:03 +0300

    Задание № 1 и №2.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import transport.Car;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void printSeparation() {
@@ -8,14 +10,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car carLada = new Car(" Lada ", "Granta", 2015,  " Россия ", " Желтый ", 0);
-        Car carAudi = new Car(" Audi ", " A8 50 L TDI quattro ", 2020, " Германия ", " Черный ",3.0);
-        Car carBMW = new Car(" BMW ", " Z8 ", 2021, " Германия ", "",3.0);
-        Car carKia = new Car(" Kia ", " Sportage 4-го поколения ", 2020, " Южная Корея ", " Красный ",2.4);
-        Car carHyundai = new Car(" Hyundai ", " Avante ", 2016, " Южная Корея ", " Оранжевый ",0);
+        Car carLada = new Car( "Lada", "Granta", 2015, "Россия", "желтый","седан",
+                false, "МКПП","и015ка199",4,3.0,
+                new Car.Key( true, true));
+        Car carAudi = new Car("Audi", "50 L TDI quattro",2020, "Германия","черный",
+                "седан", false, "Вариатор", "р889ка179",4, 3.0,
+                new Car.Key( false, false));
+        Car carBMW = new Car( "BMW", "Z8", 2021, "Германия","черный",
+                "родстер", true, "АКПП", "и007ос099", 2, 3.0,
+                new Car.Key( false, true));
+        Car carKia = new Car("Kia", "Sportage 4-го поколения",2020, "Южная Корея", "Красный",
+                "кроссовер", false, "АКПП", "а000аа37",5,2.4,
+                new Car.Key( true, false));
+        Car carHyundai = new Car( "Hyundai", "Avante",2016,"Южная Корея", "Оранжевый",
+                "седан", false, "МКПП", "с065мк199", 5, 1.6,
+                new Car.Key( true, true));
 
 
-        System.out.println(carLada);
+
+
+        System.out.println(carLada );
         printSeparation();
         System.out.println(carAudi);
         printSeparation();
--- src/transport/ValidateUtils.java
+++ src/transport/ValidateUtils.java
@@ -7,8 +7,9 @@ public class ValidateUtils {
             return (value == null || value.isBlank() || value.isEmpty()) ? defaultValue : value;
 
         }
-        public static Integer validateNum(Integer value){
-            return  value == null || value < 0 ? 0 : value;
+        public static Boolean validateBoolean(Boolean value){
+            return value != null && value;
+
 
         }
 
