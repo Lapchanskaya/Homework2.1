4fd2abfd59dfc367bc2de32cb27cbebd40db02f6
Svetlana <isaeva322@mail.ru>
2023-02-17 12:26:59 +0300

    Задание № 1 исправления

--- src/transport/Driver.java
+++ src/transport/Driver.java
@@ -1,5 +1,7 @@
 package transport;
 
+import java.util.Objects;
+
 public abstract  class Driver {
 
     private String fullName;
@@ -48,5 +50,18 @@ public abstract  class Driver {
                 ", Водительское удостоверение : " + driverLicense +
                 ", Опыт вождения: " + experience +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Driver driver = (Driver) o;
+        return driverLicense == driver.driverLicense && experience == driver.experience && Objects.equals(fullName, driver.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, driverLicense, experience);
     }
 }
--- src/transport/Main.java
+++ src/transport/Main.java
@@ -94,6 +94,7 @@ public class Main {
         DriverD driverD1 = new DriverD("Архипов Фёдор Максимович, Категории D", true, 6);
 
         DriverD driverD2 = new DriverD("Соколов Лука Александрович, Категории D", false, 2);
+        DriverB driverB3 = new DriverB("Алешин Максим Сергеевич, Категории В", true, 11);
 
 
         Mechanic mechanic1 = new Mechanic(" Лебедев Александр ", "Автолюб");
@@ -165,14 +166,16 @@ public class Main {
        drivers.add(driverC2);
        drivers.add(driverD1);
        drivers.add(driverD2);
+        drivers.add(driverB3);
+
         System.out.println(drivers);
         if (drivers.contains(driverC1) || drivers.contains(driverC2) ||drivers.contains(driverB1) || drivers.contains(driverB2) ||
                 drivers.contains(driverD1) || drivers.contains(driverD2)){
             System.out.println( " Такой водитель уже есть!");
         }
-        drivers.add(driverC1);
-        drivers.add(driverD1);
-        drivers.add(driverB2);
+        drivers.add(driverB3);
+
+
 
 
         Iterator<Object> iterDrivers = drivers.iterator();
